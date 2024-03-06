e3e5c4e291f797d0b8bba5e2d54fcacb1345ce65
Alyosha Baryshnikov <alohahoy@gmail.com>
2024-03-06 23:47:14 +0000

    make basic quoting and quote fetching functionality

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import settings
 import discord
 from discord.ext import commands
 
+import aiosqlite
 
 logger = settings.logging.getLogger("bot")
 
@@ -15,8 +16,17 @@ def run():
     async def on_ready():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
 
+        # db connection
+        async with aiosqlite.connect("main.db") as db:
+            async with db.cursor() as cursor:
+                await cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, guild INTEGER, username VARCHAR(255))')
+            await db.commit()
+
+            async with db.cursor() as cursor:
+                await cursor.execute('CREATE TABLE IF NOT EXISTS quotes (id INTEGER PRIMARY KEY AUTOINCREMENT, guild INTEGER, content VARCHAR(500), author VARCHAR(255))')
+            await db.commit()
+
     @bot.command()
-    # @commands.has_permissions(manage_messages=True)
     async def ping(ctx):
         await ctx.send('pong')
 
@@ -26,20 +36,62 @@ def run():
         await ctx.send('Help yourself you lazy shit')
 
 
+    @bot.command()
+    async def adduser(ctx, member:discord.Member):
+        async with aiosqlite.connect("main.db") as db:
+            async with db.cursor() as cursor:
+                await cursor.execute("SELECT id FROM users WHERE guild = ?", (ctx.guild.id,))
+                data = await cursor.fetchone()
+                if data:
+                    await cursor.execute("UPDATE users SET id = ? WHERE guild = ?", (member.id, ctx.guild.id,))
+                else:
+                    await cursor.execute("INSERT INTO users (id, guild, username) VALUES (?, ?, ?)", (member.id, ctx.guild.id, member.name,))
+            await db.commit()
+
     @bot.event
     async def on_message(message):
+
+        if message.author == bot.user:
+            return  # Ignore messages sent by the bot itself
+
         if message.reference and message.content.startswith('!quote'):
             # Check if the message is a reply and starts with the command prefix
             replied_message = await message.channel.fetch_message(message.reference.message_id)
-            await message.channel.send(f"Original Message: {replied_message.content}")
-        else:
-            message.channel.send("Reply to a message to quote.")
+            # await message.channel.send(f"Original Message: {replied_message.content}")
+            
+            async with aiosqlite.connect("main.db") as db:
+                async with db.cursor() as cursor:
+                    await cursor.execute("INSERT INTO quotes (guild, content, author) VALUES (?, ?,  ?)",
+                                         (message.guild.id, replied_message.content, replied_message.author.name,))
+                await db.commit()
+            # save to database
+            print(replied_message.id)
+            print("Quoted Text: " + replied_message.content)
+            print(replied_message.author)
+            
+            # else command is not working
+        elif message.content.startswith('!quote'):
+            await message.channel.send("Reply to a message to quote.")
 
         await bot.process_commands(message)
 
     @bot.command()
     async def quote(ctx):
         await ctx.send("Quoted.")
+
+
+    @bot.command()
+    async def get_quotes(ctx, member:discord.Member):
+        async with aiosqlite.connect("main.db") as db:
+            async with db.cursor() as cursor:
+                await cursor.execute("SELECT * FROM quotes WHERE author = ?", (member.name,))
+                data = await cursor.fetchall()
+                if data:
+                    print(data)
+                else:
+                    print("no quotes by " + str(member.name))
+            await db.commit()
+        
 
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
     # print(settings.DISCORD_API_SECRET)
